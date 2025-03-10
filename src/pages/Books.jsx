836a19f8b8b0065d61c8d3e57ea4b4690866854c
Pearl-Kai <purlkai92@gmail.com>
2025-03-10 06:34:53 -0700

    first commit

---
+++ src/pages/Books.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PagesLayout from "../layouts/PagesLayout";
+
+const Books = () => {
+  
+   return (
+    <PagesLayout>
+    <h1>Books</h1>
+   </PagesLayout>
+   );
+   
+  
+};
+
+export default Books;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PagesLayout from "../layouts/PagesLayout";
+const Landing = () => {
+  
+   return (
+        <PagesLayout>
+            <div>Landing</div>
+        </PagesLayout>
+   );
+   
+  
+};
+
+export default Landing;
